f60ff822699a2ac1a117715997f3e89ed193c2c8
Nirnay Majumder <105104619+Nirnay007@users.noreply.github.com>
2022-09-10 10:09:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var navLinks = document.getElementById('navLinks');
 
 function showMenu() {
-  navLinks.style.right = '0';
+  navLinks.style.right = "0";
 }
 function hideMenu() {
-  navLinks.style.right = '-200px';
+  navLinks.style.right = "-200px";
+}
+
+// https://www.youtube.com/watch?v=sGQSz22U8VM
+function sendEmail(){
+  Email.send({
+    Host : 
+  })
 }
