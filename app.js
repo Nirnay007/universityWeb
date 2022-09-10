e4abe988776c0e09d25be66df7a727e44fff7fa5
Nirnay Majumder <105104619+Nirnay007@users.noreply.github.com>
2022-09-10 10:56:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var navLinks = document.getElementById('navLinks');
 
 function showMenu() {
-  navLinks.style.right = "0";
+  navLinks.style.right = '0';
 }
 function hideMenu() {
-  navLinks.style.right = "-200px";
+  navLinks.style.right = '-200px';
 }
 
-// https://www.youtube.com/watch?v=sGQSz22U8VM
-function sendEmail(){
+function sendEmail() {
   Email.send({
-    Host : 
-  })
+    Host: 'smtp.gmail.com',
+    Username: 'majumderrniru007',
+    Password: 'janina007!',
+    To: 'majumderrniru007@gmail.com',
+    From: document.getElementById('email').value,
+    Subject: 'New Contact Form Enquiry',
+    Body:
+      'Name: ' +
+      document.getElementById('name').value +
+      '<br> Email: ' +
+      document.getElementById('email').value +
+      '<br> Subject: ' +
+      document.getElementById('subject').value +
+      '<br> Message: ' +
+      document.getElementById('message').value,
+  }).then((message) => alert('Message Sent Successfully'));
 }
